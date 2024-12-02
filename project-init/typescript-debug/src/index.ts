364912ae00522a57652f511602aea4b2288a2226
unknown <nami0879@naver.com>
2024-12-02 19:36:45 +0900

    project-init about ts

---
+++ project-init/typescript-debug/src/index.ts
@@ -0,0 +1,10 @@
+let a = 4;
+let b = a + 5;
+
+console.log('calc...2');
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum += i;
+    console.log('sum -> ', sum);
+}
---
+++ project-init/typescript-electron/src/ts/index.ts
@@ -0,0 +1,3 @@
+document.querySelector('span')?.addEventListener('click', () => {
+    alert('span !!')
+})
