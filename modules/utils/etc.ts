6c0b753b7f21c4f6dfeebe776210f0abf76865e6
unknown <nami0879@naver.com>
2025-01-08 00:07:47 +0900

    etc

---
+++ modules/utils/etc.ts
@@ -0,0 +1,15 @@
+function sleep(ms: number): Promise<void> { return new Promise((resolve) => setTimeout(resolve, ms)) }
+function getRandomNumberInRange(min: number, max: number): number { return Math.floor(Math.random() * (max - min + 1)) + min }
+
+function shuffle<T>(array: T[]): T[] {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        // [array[i], array[j]] = [array[j], array[i]]
+    }
+
+    return array
+}
